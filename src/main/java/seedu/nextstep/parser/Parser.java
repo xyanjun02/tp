c3a8e4695573d031406c581f4f9da26b8b42a6f0
Jia Jun <jiajun412@gmail.com>
2025-03-27 18:28:19 +0800

    Fixed indentation errors

--- src/main/java/seedu/nextstep/parser/Parser.java
+++ src/main/java/seedu/nextstep/parser/Parser.java
@@ -15,30 +15,30 @@ public class Parser {
     public static void processCommand(String input) {
         String[] words = input.split(" ");
         switch (words[0]) {
-            case "add":
-                new AddCommand(input).execute();
-                break;
-            case "delete":
-                new DeleteCommand(input).execute();
-                break;
-            case "list":
-                new ListCommand().execute();
-                break;
-            case "help":
-                new HelpCommand().execute();
-                break;
-            case "find/s":
-                new FindSkillCommand(input).execute();
-                break;
-            case "find/r":
-                new FindRoleCommand(input).execute();
-                break;
-            case "find/c":
-                new FindCompanyCommand(input).execute();
-                break;
-            default:
-            System.out.println("Unknown command... Type help for more information :v");
+        case "add":
+            new AddCommand(input).execute();
             break;
+        case "delete":
+            new DeleteCommand(input).execute();
+            break;
+        case "list":
+            new ListCommand().execute();
+            break;
+        case "help":
+            new HelpCommand().execute();
+            break;
+        case "find/s":
+            new FindSkillCommand(input).execute();
+            break;
+        case "find/r":
+            new FindRoleCommand(input).execute();
+            break;
+        case "find/c":
+            new FindCompanyCommand(input).execute();
+            break;
+        default:
+            System.out.println("Unknown command... Type help for more information :v");
+        break;
         }
     }
 }
